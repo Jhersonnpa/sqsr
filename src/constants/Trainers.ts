f56e2400794ac167d106382783a14f06ed974965
Jhersonnpa <jhersonnpa@gmail.com>
2024-11-19 21:11:32 +0100

    Creacion de pages/el-nostre-equip y componente Footer

---
+++ src/constants/Trainers.ts
@@ -0,0 +1,44 @@
+export const Trainers = [
+  {
+    name: "Pau",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+  {
+    name: "Guillem",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+  {
+    name: "Sandra",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+  {
+    name: "Carlota",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+  {
+    name: "Carla",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+  {
+    name: "Marian",
+    category: "Street",
+    socials: [
+
+    ]
+  },
+]
